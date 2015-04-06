d33ab094c94cda93963e0bc2dfb1be1d51ff9b2d
Javi Alzueta <jalzuetap@gmail.com>
2015-04-06 23:32:10 +0200

    Initial Commit

---
+++ Xwing/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Xwing
+//
+//  Created by Javi Alzueta on 6/4/15.
+//  Copyright (c) 2015 FillinGAPPs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
